d079b203c64196fd5ddf518499e6a439caf8bb33
Matt2334 <89859938+Matt2334@users.noreply.github.com>
2023-01-23 22:19:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ print(f"Your new link is: {short['result']['full_short_link']}")
 
 def CreateDatabase():
     name = input("What do you want the name of your database to be: ")
-    con = psycopg2.connect(user="mattodinsky")
+    con = psycopg2.connect(user="USER")
     con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT);
     cursor = con.cursor()
     cursor.execute(f"CREATE DATABASE {name}")
-    connected = psycopg2.connect(database=name, user="mattodinsky")
+    connected = psycopg2.connect(database=name, user="USER")
     mouse = connected.cursor()
     mouse.execute("CREATE TABLE short(id BIGSERIAL NOT NULL PRIMARY KEY, "
                     "website_name VARCHAR(50) NOT NULL, old_link VARCHAR(60), shortened_link VARCHAR(30))")
